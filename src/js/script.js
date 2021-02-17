79beeba630bbd066bbf0ca0460e51541392b18a4
uuse <znrfire@gmail.com>
2021-02-17 12:47:07 +0400

    Добавлен phpmailer, исправлены ошибки

--- src/js/script.js
+++ src/js/script.js
@@ -36,38 +36,36 @@ window.addEventListener('DOMContentLoaded', () => {
             modal.style.display = 'none';
         }
     });
-
-    
-
-    //Отправка данных на сервер
-    function send (event, php){
-        console.log("Отправка запроса");
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
-        var req = new XMLHttpRequest();
-        req.open('POST', php, true);
-        req.onload = function() {
-            if (req.status >= 200 && req.status < 400) {
-            json = JSON.parse(this.response); // internet explorer 11
-                console.log(json);
-                
-                // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-                if (json.result == "success") {
-                    // Если сообщение отправлено
-                    alert("Сообщение отправлено");
-                    // modal.style.display = 'flex';
-                    // modalSuccess.style.display = 'block';
-                } else {
-                    // Если произошла ошибка
-                    alert("Ошибка. Сообщение не отправлено");
-                    // modalSuccess.style.display = '';
-                    // modalError.style.display = 'block';
-                }
-            // Если не удалось связаться с php файлом
-            } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
-
-        // Если не удалось отправить запрос. Стоит блок на хостинге
-        req.onerror = function() {alert("Ошибка отправки запроса");};
-        req.send(new FormData(event.target));
-    }
 });
+
+//Отправка данных на сервер
+function send (event, php){
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response); // internet explorer 11
+            console.log(json);
+            
+            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+            if (json.result == "success") {
+                // Если сообщение отправлено
+                alert("Сообщение отправлено");
+                // modal.style.display = 'flex';
+                // modalSuccess.style.display = 'block';
+            } else {
+                // Если произошла ошибка
+                alert("Ошибка. Сообщение не отправлено");
+                // modalSuccess.style.display = '';
+                // modalError.style.display = 'block';
+            }
+        // Если не удалось связаться с php файлом
+        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() {alert("Ошибка отправки запроса");};
+    req.send(new FormData(event.target));
+}
 
